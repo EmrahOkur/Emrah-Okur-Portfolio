268610de0ba1ef08a5676239961e812d6bd3164f
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2024-12-25 14:16:40 +0100

    hero responsive

--- Emrah-Okur-Portfolio/src/app/hero/hero.component.ts
+++ Emrah-Okur-Portfolio/src/app/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
-  templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  imports: [CommonModule],
+  templateUrl: './hero.component.html', 
+  styleUrls:[ './hero.component.scss','hero-responsive.component.scss', 'hero-desktop.component.scss', 'hero-mobile.component.scss']
 })
 export class HeroComponent {
 
