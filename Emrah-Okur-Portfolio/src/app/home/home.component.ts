4a0abdd065dfdcb79a94023728ff9ed39c8d3c56
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2024-12-25 15:59:57 +0100

    about-me-section html/Scss/responsive

--- Emrah-Okur-Portfolio/src/app/home/home.component.ts
+++ Emrah-Okur-Portfolio/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../hero/hero.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,HeroComponent],
+  imports: [CommonModule,HeroComponent, AboutMeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
