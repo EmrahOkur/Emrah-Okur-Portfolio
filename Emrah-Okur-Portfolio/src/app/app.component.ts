57b0a6e53b0f72c05d48ac753d777b64b78b15b4
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2024-12-26 11:58:47 +0100

    import and install AOS function for Animation

--- Emrah-Okur-Portfolio/src/app/app.component.ts
+++ Emrah-Okur-Portfolio/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,19 @@ import { FooterComponent } from './footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Emrah-Okur-Portfolio';
+export class AppComponent implements OnInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
+  ngOnInit() {
+    // Überprüfen, ob der Code im Browser ausgeführt wird
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({
+        duration: 1200,       // Dauer der Animation in Millisekunden
+        easing: 'ease-out-back', // Art der Animation
+        once: false,          // Animation wird bei erneutem Scrollen ausgelöst
+        mirror: true,         // Animation wird auch bei Scroll nach oben ausgelöst
+        delay: 100            // Verzögerung vor dem Start der Animation
+      });
+    }
+  }
 }
--- Emrah-Okur-Portfolio/src/app/home/home.component.ts
+++ Emrah-Okur-Portfolio/src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from '../hero/hero.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
+import { SkillsSectionComponent } from '../skills-section/skills-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,HeroComponent, AboutMeComponent],
+  imports: [CommonModule,HeroComponent, AboutMeComponent,SkillsSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
