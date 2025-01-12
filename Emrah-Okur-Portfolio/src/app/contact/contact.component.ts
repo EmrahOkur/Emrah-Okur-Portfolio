95e5fdac40d3bd528403b7662fbdc08c631f6033
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2025-01-12 18:21:17 +0100

    contact-section

--- Emrah-Okur-Portfolio/src/app/contact/contact.component.ts
+++ Emrah-Okur-Portfolio/src/app/contact/contact.component.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
+import { strictEmailValidator } from './validators';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, HttpClientModule, RouterLink],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss', 'contact-responsive.scss']
 })
-export class ContactComponent {
 
-}
+  export class ContactComponent {
+    contactForm: FormGroup;
+    showConfirmation: boolean = false;
+    mailTest = true;
+    http = inject(HttpClient)
+  
+    post = {
+      endPoint: 'https://emrah-okur.com/sendMail.php',
+      body: (payload: any) => JSON.stringify(payload),
+      options: {
+        headers: {
+          'Content-Type': 'text/plain',
+          responseType: 'text',
+        },
+      },
+    };
+  
+    constructor(private fb: FormBuilder) {
+      this.contactForm = this.fb.group({
+        name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
+        email: ['', [Validators.required, Validators.email, strictEmailValidator, Validators.maxLength(40)]],
+        message: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(500)]],
+        consent: [false, Validators.requiredTrue],
+      });
+    }
+    
+    
+  
+    onSubmit(): void {
+      if (this.contactForm.valid) {
+        // HTTP-Post-Anfrage senden mit contactForm.value
+        this.http.post<any>(this.post.endPoint, this.post.body(this.contactForm.value))
+          .subscribe({
+            next: (response: any) => {
+              console.info('HTTP-Post erfolgreich:', response);
+    
+              // Bestätigungsmeldung anzeigen
+              this.showConfirmation = true;
+    
+              // Nach 3 Sekunden die Bestätigungsmeldung ausblenden
+              setTimeout(() => {
+                this.showConfirmation = false;
+              }, 3000);
+    
+              // Formular zurücksetzen
+              this.contactForm.reset();
+            },
+            error: (error: any) => {
+              console.error('Fehler beim Senden der HTTP-Post-Anfrage:', error);
+            },
+            complete: () => console.info('HTTP-Post abgeschlossen'),
+          });
+      }
+    }
+  
+    scrollToTop() {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  
+    get name() { return this.contactForm.get('name'); }
+    get email() { return this.contactForm.get('email'); }
+    get message() { return this.contactForm.get('message'); }
+    get consent() { return this.contactForm.get('consent'); }
+  }
--- Emrah-Okur-Portfolio/src/app/home/home.component.ts
+++ Emrah-Okur-Portfolio/src/app/home/home.component.ts
@@ -5,11 +5,12 @@ import { AboutMeComponent } from '../about-me/about-me.component';
 import { SkillsSectionComponent } from '../skills-section/skills-section.component';
 import { PortfolioSectionComponent } from '../portfolio-section/portfolio-section.component';
 import { ReferencesSectionComponent } from '../references-section/references-section.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,HeroComponent, AboutMeComponent,SkillsSectionComponent,PortfolioSectionComponent,ReferencesSectionComponent],
+  imports: [CommonModule,HeroComponent, AboutMeComponent,SkillsSectionComponent,PortfolioSectionComponent,ReferencesSectionComponent,ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
