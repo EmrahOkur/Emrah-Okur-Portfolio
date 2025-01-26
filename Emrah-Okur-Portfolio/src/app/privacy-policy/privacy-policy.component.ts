cbeece3af56c46236d63bb0a6d985c54a0909c97
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2025-01-26 15:52:37 +0100

    privacy fix and complete

--- Emrah-Okur-Portfolio/src/app/privacy-policy/privacy-policy.component.ts
+++ Emrah-Okur-Portfolio/src/app/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
