83a3412f509a1407326c5d1fe31c3d0f9f9f9cbc
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2025-01-28 17:50:02 +0100

    fixes

--- Emrah-Okur-Portfolio/src/app/privacy-policy/privacy-policy.component.ts
+++ Emrah-Okur-Portfolio/src/app/privacy-policy/privacy-policy.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
@@ -8,6 +9,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
-export class PrivacyPolicyComponent {
 
+export class PrivacyPolicyComponent {
+  currentLanguage: string = 'en'; 
+  constructor(private translate: TranslateService) {
+    this.currentLanguage = this.translate.currentLang || 'en';
+
+    this.translate.onLangChange.subscribe((event) => {
+      this.currentLanguage = event.lang;
+    });
+  }
 }
