b5c4b031bd2d0db375040d3b3d7cc56debf49aa8
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2025-01-30 16:49:10 +0100

    fixes

--- Emrah-Okur-Portfolio/src/app/hero/hero.component.ts
+++ Emrah-Okur-Portfolio/src/app/hero/hero.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,Inject, PLATFORM_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { isPlatformBrowser } from '@angular/common';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-hero',
@@ -9,6 +11,17 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './hero.component.html', 
   styleUrls:[ './hero.component.scss','hero-responsive.component.scss', 'hero-desktop.component.scss', 'hero-mobile.component.scss','hero-tablet.component.scss']
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
 
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({
+        duration: 1000, 
+        once: false, 
+      });
+    }
+  }
 }
+
